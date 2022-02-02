d16d6baa58a8d317abbc783e41c502984d22a3bc
MagnoTheFirst <alejandro.r.laneri@gmail.com>
2022-02-03 00:30:28 +0100

    Contact Logic implemented

---
+++ js/contact-logic.js
@@ -0,0 +1,19 @@
+function postContactEntry(obj) {
+    var url = 'http://alejandro-laneri.internet-box.ch:8085/api/v1/contact/entry'
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("POST", url);
+    xmlHttp.setRequestHeader("Accept", "application/json");
+    xmlHttp.setRequestHeader("Content-Type", "application/json");
+    xmlHttp.send(obj);
+
+}
+
+function getFieldContent() {
+    var contactEntry = {};
+    contactEntry.name = document.getElementById('name').value;
+    contactEntry.email = document.getElementById('email').value;
+    contactEntry.comment = document.getElementById('comments').value;
+    postContactEntry(JSON.stringify(contactEntry));
+    alert(JSON.stringify(contactEntry));
+
+}
